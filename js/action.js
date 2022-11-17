6869d74a8c0d12daaa98bea9dfbc3cbd27c18b65
Daniel <dev@dwennemar.de>
2022-11-16 22:21:55 -0500

    add member 2 tasks

--- js/action.js
+++ js/action.js
@@ -1,3 +1,14 @@
+var pieces = [
+  [0, -1, 0, -1, 0, -1, 0, -1],
+  [-1, 0, -1, 0, -1, 0, -1, 0],
+  [0, -1, 0, -1, 0, -1, 0, -1],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1, 0, 1, 0],
+];
+
 function getCellID(colsPerRow, r, c) {
   const id = r * colsPerRow + c;
   return `c${id};`
@@ -17,6 +28,20 @@ function drawGameBoard(rows, columns) {
       col.setAttribute("class", "white");
       col.setAttribute("id", getCellID(8, i, j))
       row.appendChild(col);
+    }
+  }
+}
+
+function showGamePieces(rows, columns) {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const cellId = getCellID(8,i,j);
+      const cell = document.getElementById(cellId);
+      if (pieces[i][j] < 0) {
+        cell.setAttribute("class", "redpiece");
+      } else if (pieces[i][j] > 0) {
+        cell.setAttribute("class", "blackpiece");
+      }
     }
   }
 }
