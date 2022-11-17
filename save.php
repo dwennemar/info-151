fd7892f06cc6acdcd6209b69a4ddc505f904964b
Daniel <dev@dwennemar.de>
2022-11-17 09:46:04 -0500

    add honesty statements and stylesheets

--- save.php
+++ save.php
@@ -8,8 +8,40 @@
     $result = "{$program}, {$plength},{$fav1}{$fav2}{$fav3}";
 
     $file = fopen("data/survey.txt", "a");
-    fwrite($file, "{$result}\n");
+    $result = fwrite($file, "{$result}\n");
 
     echo "Data saved successfully";
     fclose($file);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Save Survey</title>
+    <link rel="stylesheet" type="text/css" href="./styles/save.css" media="all">
+</head>
+<body>
+<span id="message">
+    <?php
+        echo $result ? "Data saved successfully" : "Error while saving your data"
+    ?>
+</span>
+<div class="honesty-statement">
+    <span>I certify that: </span>
+    <ul>
+        <li>My work will be entirely my own work.</li>
+        <li>I will not quote the words of any other person from a printed source or a website without indicating what has been quoted and providing an appropriate citation.</li>
+        <li>I will not submit my work in this course to satisfy the requirements of any other course.</li>
+    </ul>
+    <p>Full Name: <em>Daniel Wennemar</em></p>
+    <p>Date: 11/17/2022</p>
+</div>
+<a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fwww.cs.drexel.edu%2F%7Edw967%2Fsave.php">W3C Validator checked!</a>
+<a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fwww.cs.drexel.edu%2F%7Edw967%2Fsave.php&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en">
+    <img style="border:0;width:88px;height:31px"
+         src="https://jigsaw.w3.org/css-validator/images/vcss-blue"
+         alt="Valid CSS!">
+</a>
+</body>
+</html>
 
